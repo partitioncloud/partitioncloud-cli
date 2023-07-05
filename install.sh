1c40fcb582eb210555c06adf344a1ea6aea96b15
augustin64 <me.git@augustin64.fr>
2023-07-05 19:28:41 +0200

    Fix installation warning not correctly displayed

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mkdir -p "$HOME/.local/bin"
 cp partitioncloud-cli.py $HOME/.local/bin/partitioncloud-cli
 echo "Script copied to $HOME/.local/bin/partitioncloud-cli"
 
-if [[ *"$HOME/.local/bin"* != "$PATH" ]]; then
+if [[ "$PATH" != *"$HOME/.local/bin"* ]]; then
     printf "\x1b[31mWARNING !\x1b[0m $HOME/.local/bin is not in PATH, make sure to add it to have a quick access to the script\n";
 fi;
